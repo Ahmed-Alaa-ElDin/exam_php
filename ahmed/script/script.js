66665c0b48be2e5fef38046f54ec11e36d08f94a
Ahmed-Alaa-ElDin <ahmedalaaaldin100@gmail.com>
2020-10-13 06:28:56 +0200

    finish MCQ

--- ahmed/script/script.js
+++ ahmed/script/script.js
@@ -127,7 +127,7 @@ $(function(){
     }
     // end:: check if the question is Short Answer/Essay
 
-    // begin:: check if the question is Short Answer/Essay
+    // begin:: check if the question is MultiChoice
     else if ($(this).val() == 2) {
 
       $("#MultiChoice").fadeIn()
@@ -160,6 +160,7 @@ $(function(){
         });
       })
 
+      // handle save form clicking
       $("#saveForm").on("click",function () {
         var academicYearID = $("#AcademicYear").val();
         var academicYear = $("#AcademicYear").find("option:selected").text();
@@ -201,9 +202,6 @@ $(function(){
           choices.push(choice)
         })
 
-        // console.log(mcqAllImages);
-        console.log(choices);
-
         var data = {
           "grade_id" : gradeID,
           "grade_name" : gradeName,
@@ -228,13 +226,18 @@ $(function(){
 
         $(".preview_body").children().remove()
 
-        var questionHTML = $('#QuestionEditor').summernote('code')
         var randomizeOptions = $("#mcqRandomizeOptions").prop("checked")
         var allowAttachment = $("#mcqAllowAttach").prop("checked")
         var allowPartialCredit = $("#mcqAllowPartialCredit").prop("checked")
         var maximumMarks = $("#mcqMaximumMarks").val();
         var maximumTime = $("#mcqMaximumTime").val();
 
+
+        var choices = []
+
+
+
+        // Set Question Images
         var questionImages = {}
         $("#mcqAttachFilesWithQuestion").find(".dz-preview").each(function(){
           if ($(this).find(".dz-error-message").text() == "") {
@@ -242,8 +245,22 @@ $(function(){
           }
         })
 
-        var choices = []
+        var questionImageDiv = "<div class='question_images'></div>"
+        $(".preview_body").append(questionImageDiv)
 
+        $.each(questionImages, function(imgName,imgBase64){
+          let image = "<img src=" + imgBase64 + " alt=" + imgName + ">"
+          $(".preview_body .question_images").append(image)
+        })
+        // ----------------------------------------------------------
+
+        // Set Question Text
+        var questionHTML = $('#QuestionEditor').summernote('code')
+        var questionParagraph = "<div class='question_text'>" + questionHTML + "</div>"
+        $(".preview_body").append(questionParagraph)
+        // ----------------------------------------------------------
+
+        // Set Choices
         $("#MultiChoice table .choice").each(function () {
 
           let choice = {}
@@ -256,50 +273,27 @@ $(function(){
           choices.push(choice)
         })
 
-        // set question images
-        var questionImage = "<div class='question_images'></div>"
-        $(".preview_body").append(questionImage)
-        $("#essayAttachFilesWithQuestion").find(".dz-preview").each(function(){
-          if ($(this).find(".dz-error-message").text() == "") {
-            let image = "<img src=" + allImages[$(this).find("img").attr("alt")] + " alt=" + $(this).find("img").attr("alt") + ">"
-            $(".preview_body .question_images").append(image)
+        console.log(choices);
+
+        var totalChoices = "<div class='totalChoices'></div>"
+        $(".preview_body").append(totalChoices)
+
+        $.each(choices, function (index , object) {
+          console.log(index, object);
+          let choiceBox = "<div class='choiceBox row' data-val='" + object["text"] + "'></div>"
+          $(".preview_body .totalChoices").append(choiceBox)
+
+          // Image & Text
+          if (object["image"] != undefined && object["text"] != "") {
+            let choiceImg = "<div class='col-4 choiceImg'><img src='" + object["image"] + "' alt='" + object["image_name"] + "'></div>"
+            let choiceText = "<div class='col-8 choiceText'><span>" + object["text"] + "</span></div>"
+            $(".preview_body .totalChoices .choiceBox").append(choiceImg).append(choiceText)
           }
         })
-
-        // set question text
-        var questionHTML = $('#QuestionEditor').summernote('code')
-        var questionParagraph = "<div class='question_text'>" + $('#QuestionEditor').summernote('code') + "</div>"
-        $(".preview_body").append(questionParagraph)
-
-        // set student textbox
-        if ($("#essayAllowRichText").prop("checked")) {
-          var studentAnswer = "<div class='summernote' style='max-width:90%; margin:10px auto;'><div id='StudentAnswer'></div></div>"
-          $(".preview_body").append(studentAnswer)
-            $('#StudentAnswer').summernote();
-        } else {
-          var studentAnswer = "<textarea class='student_answer'></textarea>"
-          $(".preview_body").append(studentAnswer)
-        }
-
-        // set attachment
-        if ($("#essayAllowAttach").prop("checked")) {
-          var studentAttachment = "<div class='dropzone dropzone-default dropzone-success' id='StudentAttachment'><div class='dropzone-msg dz-message needsclick'><h3 class='dropzone-msg-title'>Drop files here or click to upload.</h3><span class='dropzone-msg-desc'>Only image files are allowed for upload</span></div></div>"
-          $(".preview_body").append(studentAttachment)
-            $(".preview_body .dropzone").last().dropzone({
-              url: "/",
-              acceptedFiles: "image/*",
-              addRemoveLinks: true,
-            });
-        }
+        console.log(choices);
       })
-
     }
-    // end:: check if the question is Short Answer/Essay
+    // end:: check if the question is MultiChoice
 
   })
-
-    $(".preview_close").on("click", function () {
-      $(".preview").fadeOut()
-      $(".preview .preview_body").children().remove()
-    })
   })
