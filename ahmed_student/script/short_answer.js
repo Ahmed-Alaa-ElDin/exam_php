f430664d87d6708a1dabd5e14a7d816a0d3c16d4
Dr Ahmed Alaa <a.alaa@EL-EZABYGROUP.LOCAL>
2020-10-27 16:15:20 +0200

    commit

--- ahmed_student/script/short_answer.js
+++ ahmed_student/script/short_answer.js
@@ -3,7 +3,7 @@ $(function () {
   $(document).on('contextmenu', e => e.preventDefault());
 
   // JSON input
-  var json_input = {"academic_id":"1","academic_year":"2020 - 2021","grade_id":"","grade_name":"","subject_id":"2","subject_name":"Urdu","topic_name":"","filter_tags":[],"question_html":"What's the color of banana ??","question_details":"<p>Explain it</p>","allow_rich_text":false,"allow_attachment":true,"maximum_marks":"15","words_count_limit":"10","maximum_time":"10","question_images":{}}
+  var json_input = { "academic_id": "1", "academic_year": "2020 - 2021", "grade_id": "", "grade_name": "", "subject_id": "2", "subject_name": "Urdu", "topic_name": "", "filter_tags": [], "question_html": "What's the color of banana ??", "question_details": "<p>Explain it</p>", "allow_rich_text": false, "allow_attachment": true, "maximum_marks": "15", "words_count_limit": "10", "maximum_time": "10", "question_images": {} }
 
   // Check Language
   if (json_input.subject_id == "0") {
@@ -11,23 +11,23 @@ $(function () {
       MakeTextBoxUrduEnabled(answerTextarea)
       MakeTextBoxUrduEnabled(answerSummernote)
     })
-    $("#answerTextarea").attr("dir","rtl")
-  } else 
-  if (json_input.subject_id == "2") {
-    $.getScript("ahmed_student/script/arabictextbox.js", function () {
-      MakeTextBoxArabicEnabled(answerTextarea)
-      MakeTextBoxUrduEnabled(answerSummernote)
-    })
-    $("#answerTextarea").attr("dir","rtl")
-  }
+    $("#answerTextarea").attr("dir", "rtl")
+  } else
+    if (json_input.subject_id == "2") {
+      $.getScript("ahmed_student/script/arabictextbox.js", function () {
+        MakeTextBoxArabicEnabled(answerTextarea)
+        MakeTextBoxUrduEnabled(answerSummernote)
+      })
+      $("#answerTextarea").attr("dir", "rtl")
+    }
 
   // starter worder count
   $(".wordsCount").text(`${json_input.words_count_limit} Words Left`);
 
   // add time alert
-  setInterval(function(){
+  setInterval(function () {
     let minutes = $(".countdown").text().split(":")[1]
-    minutes < 1 ? $(".alert-danger").css("display","block") : $(".alert-danger").css("display","none")
+    minutes < 1 ? $(".alert-danger").css("display", "block") : $(".alert-danger").css("display", "none")
   }, 1000);
 
   // set question text
@@ -36,8 +36,8 @@ $(function () {
 
   // set answer text area
   if (json_input.allow_rich_text) {
-    $("#answerSummernote").siblings("textarea").css("display","none")
-    $('.note-editor .note-editable').on("keydown", function(e) {
+    $("#answerSummernote").siblings("textarea").css("display", "none")
+    $('.note-editor .note-editable').on("keydown", function (e) {
       var words = $(this).html().split('</p><p>').join(' ').split(" ");
       $(".wordsCount").text(`${json_input.words_count_limit - words.length} Words Left`);
       // console.log(e.keyCode);
@@ -46,8 +46,8 @@ $(function () {
       }
     });
   } else {
-    $("#answerSummernote").siblings(".note-editor.note-frame.card").css("display","none")
-    $("#answerSummernote").siblings("textarea").on("keydown", function(e) {
+    $("#answerSummernote").siblings(".note-editor.note-frame.card").css("display", "none")
+    $("#answerSummernote").siblings("textarea").on("keydown", function (e) {
       var words = $(this).val().split(' ');
       $(".wordsCount").text(`${json_input.words_count_limit - words.length} Words Left`);
       if ((json_input.words_count_limit - words.length) == 0 && e.keyCode == 32) {
@@ -58,25 +58,25 @@ $(function () {
 
   // set attachment
   if (!json_input.allow_attachment) {
-    $("#answerAttachments").css("display","none")
+    $("#answerAttachments").css("display", "none")
   }
 
   // make json 
-  $("#submitAnswer").on("click",function () {
+  $("#submitAnswer").on("click", function () {
 
-    var answerText = json_input.allow_rich_text ? $('#answerSummernote').siblings(".note-editor").find(".note-editable.card-block").html(): $('#answerSummernote').siblings("textarea").val();
-    
+    var answerText = json_input.allow_rich_text ? $('#answerSummernote').siblings(".note-editor").find(".note-editable.card-block").html() : $('#answerSummernote').siblings("textarea").val();
+
     // Make Screenshot
-    html2canvas(document.getElementById("answer"),{"y":"140", "imageTimeout":0}).then(function(canvas) {
+    html2canvas(document.getElementById("answer"), { "y": "140", "imageTimeout": 0 }).then(function (canvas) {
 
       output = {
-        "answer_text" : answerText,
-        "screenshot" : canvas.toDataURL()
+        "answer_text": answerText,
+        "screenshot": canvas.toDataURL()
       }
       outputJSON = JSON.stringify(output)
       console.log(output);
 
-     });
+    });
   })
 
 })
