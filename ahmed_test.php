8d9c2e6208d5a2fe759817c102f22679e3fa1018
Dr Ahmed Alaa <a.alaa@EL-EZABYGROUP.LOCAL>
2020-11-02 15:32:06 +0200

    upload trial

--- ahmed_test.php
+++ ahmed_test.php
@@ -1,3 +1,10 @@
 <?php
-var_dump($_POST)
- ?>
+var_dump($_FILES);
+if (isset($_FILES) && !empty($_FILES)) {
+    $answer_attachment_name = $_FILES["file"]["name"];
+    $answer_attachment_old_loc = $_FILES["file"]["tmp_name"];
+    $answer_attachment_new_loc = $_SERVER['DOCUMENT_ROOT'] . "/cis/attachments/" . rand(0,999999) . $answer_attachment_name;
+    move_uploaded_file($answer_attachment_old_loc,$answer_attachment_new_loc);
+}
+echo ("ahmed");
+?>
